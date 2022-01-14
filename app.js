ce4439ef9768f1e36812aa4835167a704900ad72
Mert Efe Cerit <mertefecerit@gmail.com>
2022-01-14 11:37:36 +0300

    remove src dir. and ejs, ejs lay. install

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const { config } = require('dotenv');
 const { database } = require('./database');
+const expressLayouts = require('express-ejs-layouts');
+
+
 const app = express();
 
 config();
 database();
+
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.static('public'));
+app.use(expressLayouts);
+app.use(express.urlencoded({extended: true}));
 
 app.listen(process.env.APP_PORT, () => {
     console.log(`Server started on http://localhost:${process.env.APP_PORT}`);

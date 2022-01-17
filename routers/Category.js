3cfecaba0edec846621aa3d31bd6c492c92bfda0
Mert Efe Cerit <mertefecerit@gmail.com>
2022-01-17 14:40:35 +0300

    for auth service prepare

--- routers/Category.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const CategoryController = require('../controllers/Category');
-
-router.get('/',CategoryController.list);
-router.post('/', CategoryController.create);
-
-module.exports = router;
--- routers/Course.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const CourseController = require('../controllers/Course');
-
-router.post('/', CourseController.create);
-
-module.exports = router;
